717e7c779cc4ec23b338576ac10de4f5e96fff20
rodolfo-tenfen <rodolfo.tenfen@hotmail.com>
2017-10-27 21:51:50 -0200

    Implement an adapter for SimplifidMovie and Android list item

--- app/src/main/java/com/pamplona/rodolfo/arctouchchallenge/ui/MovieItemAdapter.java
+++ app/src/main/java/com/pamplona/rodolfo/arctouchchallenge/ui/MovieItemAdapter.java
@@ -1,25 +1,32 @@
 package com.pamplona.rodolfo.arctouchchallenge.ui;
 
 import android.content.Context;
-import android.support.annotation.NonNull;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.pamplona.rodolfo.arctouchchallenge.R;
-import com.pamplona.rodolfo.arctouchchallenge.model.Movie;
+import com.pamplona.rodolfo.arctouchchallenge.data.SimplifiedMovie;
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
+/**
+ * Class responsible for adapting the simplified representation of Movies to a list of movie's item
+ */
 public class MovieItemAdapter extends BaseAdapter {
-    private final List<Movie> mData;
+    private final Context mContext;
+
+    private final List<SimplifiedMovie> mData;
     private final LayoutInflater mInflater;
 
-    public MovieItemAdapter(@NonNull Context context, @NonNull List<Movie> movies) {
-        mData = movies;
+    public MovieItemAdapter(Context context, List<SimplifiedMovie> movies) {
+        mContext = context;
 
+        mData = movies;
         mInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
     }
 
@@ -42,15 +49,25 @@ public class MovieItemAdapter extends BaseAdapter {
     public View getView(int i, View view, ViewGroup viewGroup) {
         if (view == null) view = mInflater.inflate(R.layout.movie_row, null);
 
-        TextView tvTitle = (TextView) view.findViewById(R.id.tv_title);
-        TextView tvYear = (TextView) view.findViewById(R.id.tv_year);
-        TextView tvGenres = (TextView) view.findViewById(R.id.tv_genre);
+        TextView tvTitle = view.findViewById(R.id.tv_title);
+        TextView tvYear = view.findViewById(R.id.tv_release_date);
+        TextView tvGenres = view.findViewById(R.id.tv_genre);
 
-        Movie m = mData.get(i);
+        ImageView ivPoster = view.findViewById(R.id.iv_poster);
+
+        SimplifiedMovie m = mData.get(i);
 
         tvTitle.setText(m.title());
         tvYear.setText(m.releaseDate());
         tvGenres.setText(m.genres());
+
+        int hx = ivPoster.getLayoutParams().height;
+
+        Picasso.with(mContext)
+                .load("https://image.tmdb.org/t/p/w640" + m.posterPath())
+                .resize(hx, hx)
+                .centerInside()
+                .into(ivPoster);
 
         return view;
     }
